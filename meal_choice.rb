c20579732ab258cb3ea2ee5c9f285ec02a9f2dca
Glenn Jensen <g.e.jensen@gmail.com>
2020-07-28 17:21:58 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,9 +1,13 @@
 # meal choice labs
 def meal_choice(vegetable, starch, protien="meat")
 
+<<<<<<< HEAD
 meal = "A plate of #{protien} with #{vegetable} and #{starch}."
 puts "What a nutritious meal!"
 puts meal
 meal
+=======
+"A plate of #{protien} with #{vegetable} and #{starch}."
+>>>>>>> 75210355b1ed23e596d090006ea36bd59765e270
 
 end
